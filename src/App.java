42e2e7fb8e5b7bb661abc9a79d3b1dcfc9bec686
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-18 08:53:27 +0200

    Börjat

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Vem e du jao?");
+        String jag = tb.nextLine();
+        
+        String förnamn = jag.substring(0,3);
+}
